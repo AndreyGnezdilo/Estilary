4f7f3f1cb5479eee81a918fd47dc9f398f5f1f5b
Andrey <gnezdiloandrey@gmail.com>
2023-11-26 13:44:15 +0800

    Обновил ссылку на FB2

--- Estilary.py
+++ Estilary.py
@@ -2,48 +2,45 @@ import telebot
 from telebot import types
 import os
 
-# Замените это на ваш реальный токен бота
+# Получение токена бота из переменной окружения
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Приветствуем вас! 🎈 Благодарим за доверие и покупку продукции бренда ESTILARY! Мы приготовили для вас подарки — познавательные и увлекательные электронные книги. Уверены, они подарят вам приятные минуты чтения! 📖")
     bot.send_message(message.chat.id, "Надеемся, что вы остались довольны покупкой! Оставьте, пожалуйста, отзыв на Wildberries.")
-    send_book_format_options(message)
+    send_books(message)
 
-# Функция для отправки вариантов форматов книг
-def send_book_format_options(message):
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add('PDF', 'EPUB', 'FB2')
-    bot.send_message(message.chat.id, "Вот подборка интересных книг для вас. Выберите предпочитаемый формат для загрузки:", reply_markup=markup)
-    bot.register_next_step_handler(message, send_books)
-
-# Функция для отправки книг в выбранном формате
 def send_books(message):
-    chosen_format = message.text
-    # Здесь код для отправки книг в выбранном формате
-    # Например, если выбран PDF:
-    if chosen_format == 'PDF':
-        send_books_in_pdf(message)
-    elif chosen_format == 'EPUB':
-        send_books_in_epub(message)
-    elif chosen_format == 'FB2':
-        send_books_in_fb2(message)
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    btn_pdf = types.InlineKeyboardButton("PDF", callback_data='pdf')
+    btn_epub = types.InlineKeyboardButton("EPUB", callback_data='epub')
+    btn_fb2 = types.InlineKeyboardButton("FB2", callback_data='fb2')
+    markup.add(btn_pdf, btn_epub, btn_fb2)
 
-# Функции для отправки книг в разных форматах
-def send_books_in_pdf(message):
-    # Здесь код для отправки книг в формате PDF
-    pass
+    bot.send_message(message.chat.id, "Выберите формат книги:", reply_markup=markup)
 
-def send_books_in_epub(message):
-    # Здесь код для отправки книг в формате EPUB
-    pass
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    bot.answer_callback_query(callback_query_id=call.id)
 
-def send_books_in_fb2(message):
-    # Здесь код для отправки книг в формате FB2
-    pass
+    if call.data == 'pdf':
+        bot.send_document(call.message.chat.id, 'https://github.com/AndreyGnezdilo/Estilary/raw/main/books/Primachenko_O._Bestselleryisa._K_Sebe_Nejno_Kniga_O_Tom_.a6.pdf')
+        bot.send_document(call.message.chat.id, 'https://github.com/AndreyGnezdilo/Estilary/raw/main/books/Rogov_A._Gid_Po_Stilyu.a6.pdf')
+    elif call.data == 'epub':
+        bot.send_document(call.message.chat.id, 'https://github.com/AndreyGnezdilo/Estilary/raw/main/books/Primachenko_O._Bestselleryisa._K_Sebe_Nejno_Kniga_O_Tom_.epub')
+        bot.send_document(call.message.chat.id, 'https://github.com/AndreyGnezdilo/Estilary/raw/main/books/Rogov_A._Gid_Po_Stilyu.epub')
+    elif call.data == 'fb2':
+        bot.send_document(call.message.chat.id, 'https://github.com/AndreyGnezdilo/Estilary/raw/3ff7aa1dd84a120ade77d72c1bb2888cda6ddf11/books/Primachenko_O._Bestselleryisa._K_Sebe_Nejno_Kniga_O_Tom_.fb2')
+        bot.send_document(call.message.chat.id, 'https://github.com/AndreyGnezdilo/Estilary/raw/3ff7aa1dd84a120ade77d72c1bb2888cda6ddf11/books/Rogov_A._Gid_Po_Stilyu.fb2')
 
-# Запуск бота
+    send_contact_option(call.message)
+
+def send_contact_option(message):
+    markup = types.InlineKeyboardMarkup()
+    contact_button = types.InlineKeyboardButton("Связаться с менеджером", url="https://t.me/YOUR_TELEGRAM_ACCOUNT")
+    markup.add(contact_button)
+    bot.send_message(message.chat.id, "Если у вас есть какие-либо вопросы - напишите нам", reply_markup=markup)
+
 bot.infinity_polling()
